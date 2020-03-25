6640800042a3cd6b07e13986a41a7fa13ac0ab77
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-03-25 13:37:02 +0100

    add copy

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 const canvas = document.getElementById('canvas');
-let topLeft = document.getElementById('top-left');
-let topLeftAngle = document.getElementById('top-left-angle');
+const codeToCopy = document.getElementById('code-to-copy');
 
-let leftRadius = '';
+const topLeft = document.getElementById('top-left');
+const displayTopLeft = document.getElementById('display-top-left');
+
+const topLeftAngle = document.getElementById('top-left-angle');
+const displayTopLeftAngle = document.getElementById('display-top-left-angle');
+
+let leftRadius = 0;
+let leftAngleRadius = 0;
+
 
 function resultDisplay() {
 
-  canvas.style.borderRadius = leftRadius + 'px';
-
+  if (leftAngleRadius > 0) {
+    canvas.style.borderRadius = leftAngleRadius + '% /' + leftRadius + 'px';
+    codeToCopy.innerHTML = `border-radius: ${leftAngleRadius}% / ${leftRadius} px;`;
+  } else {
+    canvas.style.borderRadius = leftRadius + 'px';
+    codeToCopy.innerHTML = `border-radius: ${leftRadius} px;`;
+  };
 };
 
-
-
-  document.addEventListener('input', (e) => {
-    switch (e.target) {
-      case topLeft:
-        leftRadius = e.target.value;
-        break;
-      case topLeftAngle:
-        console.log('wow');
-        
+document.addEventListener('input', (e) => {
+  switch (e.target) {
+    case topLeft:
+    case displayTopLeft:
+      leftRadius = e.target.value;
+      break;
+    case topLeftAngle:
+    case displayTopLeftAngle:
+      leftAngleRadius = e.target.value;
+      break;
     }
 
-    
-    resultDisplay();	
-  });
+  resultDisplay();	
+});
 
+// CODE TO COPY
+  const button = document.getElementById('button');
 
+function copyCode() {
+  codeToCopy.select();
+  document.execCommand('copy');
+  button.innerHTML = "Copied !";
+  setTimeout(() => {button.innerHTML = "Copy your code"}, 3500);
+};
 
 
